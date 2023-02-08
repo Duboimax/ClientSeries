8a3636f7cbce00032ad57dae7bd56185007cc50f
duboimax <maxencedubois22@gmail.com>
2023-02-08 09:27:08 +0100

    Ajoutez des fichiers projet.

---
+++ ClientSeriesV1/Models/Series.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSeriesV1.Models
+{
+    public class Series
+    {
+        private int serieId;
+        private string title;
+        private string resume;
+        private int nbSaisons;
+        private int nbEpisodes;
+        private int anneeCreation;
+        private string network;
+
+        public Series(int serieId, string title, string resume, int nbSaisons, int nbEpisodes, int anneeCreation, string network)
+        {
+            SerieId = serieId;
+            Title = title;
+            Resume = resume;
+            NbSaisons = nbSaisons;
+            NbEpisodes = nbEpisodes;
+            AnneeCreation = anneeCreation;
+            Network = network;
+        }
+
+        public Series() { }
+
+        public int SerieId { get => serieId; set => serieId = value; }
+        public string Title { get => title; set => title = value; }
+        public string Resume { get => resume; set => resume = value; }
+        public int NbSaisons { get => nbSaisons; set => nbSaisons = value; }
+        public int NbEpisodes { get => nbEpisodes; set => nbEpisodes = value; }
+        public int AnneeCreation { get => anneeCreation; set => anneeCreation = value; }
+        public string Network { get => network; set => network = value; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Series series &&
+                   SerieId == series.SerieId &&
+                   Title == series.Title &&
+                   Resume == series.Resume &&
+                   NbSaisons == series.NbSaisons &&
+                   NbEpisodes == series.NbEpisodes &&
+                   AnneeCreation == series.AnneeCreation &&
+                   Network == series.Network;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(SerieId, Title, Resume, NbSaisons, NbEpisodes, AnneeCreation, Network);
+        }
+    }
+}
---
+++ ClientSeriesV1/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using ClientSeriesV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSeriesV1.Services
+{
+    public interface IService
+    {
+        Task<Series> GetSeriesAsync(string nomControleur, int id);
+        Task<bool> PutSeriesAsync(string nomControleur);
+        Task<bool> PostSeriesAsync(string nomControleur);
+        Task<bool> DeleteSeriesAsync(string nomControleur);
+
+    }
+}
